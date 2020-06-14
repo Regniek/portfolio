17ae053a79fcbd188081cc2d9bebac91c0c13bdb
Juan Espitia <juanespitia92@hotmail.com>
2020-06-13 22:17:28 -0500

    Cambios generales en el portafolio

--- routes.js
+++ routes.js
@@ -1,15 +1,19 @@
 const routes = [
     {
         path: '/',
-        template: '<h1>Estoy Trabajando en la Actualizacion</h1>'
+        template: '<h2>"We were created to create what no one has created"</h2><h3>- Juan Espitia</h3>'
     },
     {
-        path: '/contacto',
-        template: '<h1>Contacto mi correo r@micorreo.com</h1>'        
+        path: '/blog',
+        template: '<h1>textos</h1>'        
     },
     {
         path: '/about',
-        template: '<h1>About Me</h1>'   
+        template: '<h1>About Me</h1><p>Web developer - Mechanic Engineer - Reporting & CPU Engineer at General Motors Colmotores, VBA, JavaScript, Noob Blogger, Fearless Gamer and Cyclist.<br /></p><p>Bienvenido a mi ser, soy Juan Camilo Espitia Camacho, Web developer y Reporting & CPU Engineer de General Motors colomotores en la ciudad de Bogota, Ingeniero Mecanico de la Universidad Distrital Fracisco Jose de Caldas apasionado por todo tipo de bicicletas, soy padre sustituto de un niño y una niña y esposo de una mujer grandiosa, me enamore del desarrollo de software a mis 15 años pero por casualidades de la vida lo deje por mas de 10 años, a mis 28 años lo retomo y con la ayuda de Platzi y mi familia sere uno de los mejores, este sitio esta creado para empezar mi camino como Blogger y servir a la comunidad.</p>'   
+    },
+    {
+        path: '/projects',
+        template: '<h1>Estos son mis proyectos</h1>'   
     },
 ];
 
