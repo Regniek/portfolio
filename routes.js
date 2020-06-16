e6a7e935a484b70941a6d4ee43925f731bae2b15
Juan Espitia <juanespitia92@hotmail.com>
2020-06-15 20:25:59 -0500

    Agregue cambios al Css y modifique algunos botones

--- routes.js
+++ routes.js
@@ -1,7 +1,7 @@
 const routes = [
     {
         path: '/',
-        template: '<h2>"We were created to create what no one has created"</h2><h3>- Juan Espitia</h3>'
+        template: '<section class="intro"><div class="intro__container"><h2>"We were created to create what no one has created"</h2><h3>- Juan Espitia</h3></div></section>'
     },
     {
         path: '/blog',
@@ -9,7 +9,7 @@ const routes = [
     },
     {
         path: '/about',
-        template: '<h1>About Me</h1><p>Web developer - Mechanic Engineer - Reporting & CPU Engineer at General Motors Colmotores, VBA, JavaScript, Noob Blogger, Fearless Gamer and Cyclist.<br /></p><p>Bienvenido a mi ser, soy Juan Camilo Espitia Camacho, Web developer y Reporting & CPU Engineer de General Motors colomotores en la ciudad de Bogota, Ingeniero Mecanico de la Universidad Distrital Fracisco Jose de Caldas apasionado por todo tipo de bicicletas, soy padre sustituto de un niño y una niña y esposo de una mujer grandiosa, me enamore del desarrollo de software a mis 15 años pero por casualidades de la vida lo deje por mas de 10 años, a mis 28 años lo retomo y con la ayuda de Platzi y mi familia sere uno de los mejores, este sitio esta creado para empezar mi camino como Blogger y servir a la comunidad.</p>'   
+        template: '<section class="about"><h1>¿Quien Soy?</h1><div class="about__intro"><p>Web developer - Mechanic Engineer - Reporting & CPU Engineer at General Motors Colmotores, VBA, JavaScript, Noob Blogger, Fearless Gamer🕹 and Cyclist🚲.<br /></p></div><div class="about__second"><p>Bienvenido a mi ser, soy Juan Camilo Espitia Camacho, Web developer y Reporting & CPU Engineer de General Motors colomotores en la ciudad de Bogota, Ingeniero Mecanico de la Universidad Distrital Fracisco Jose de Caldas apasionado por todo tipo de bicicletas, soy padre sustituto de un niño y una niña y esposo de una mujer grandiosa, me enamore del desarrollo de software a mis 15 años pero por casualidades de la vida lo deje por mas de 10 años, a mis 28 años lo retomo y con la ayuda de Platzi y mi familia sere uno de los mejores, este sitio esta creado para empezar mi camino como Blogger y servir a la comunidad.</p><h2>Mi Stack</h2><ol class="lista1"><li>✔ HTML</li><li>✔ CSS</li><li>✔ JavaScript</li></ol></div></section>'   
     },
     {
         path: '/projects',
