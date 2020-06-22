7733e39c813a216f1ec20fdb1847da3f9281437a
Juan Espitia <juanespitia92@hotmail.com>
2020-06-22 09:38:17 -0500

    Se inserta todo el Pug y se inicia el SASS

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-const router = new Router(routes);
+const menu=document.querySelector('.menu');
+console.log(menu);
+const boton=document.querySelector('#boton');
+console.log(boton);
+
+boton.addEventListener('click', hideShow);
+function hideShow(){
+    if(menu.classList.contains('menu_aparece')){
+        menu.classList.remove('menu_aparece');
+    }else{
+        menu.classList.add('menu_aparece');
+    }
+}
+
